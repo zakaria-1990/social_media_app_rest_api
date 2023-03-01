2a378bc8b4d1eaa86003683d12fc0c1a385c5fa6
zakaria oubaid <zakaria.monerobit@gmail.com>
2023-03-01 11:14:01 +0100

    initial commit

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .routers import user, post, auth, vote
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(auth.router)
+app.include_router(vote.router)
+
+
+
+
+
+
+
+
---
+++ app/oauth2.py
@@ -0,0 +1,45 @@
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from . import schemas, models
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+from .database import get_db
+from sqlalchemy.orm import Session
+from .config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({'exp': expire})
+    encoded_jwd = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwd
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        id: str = payload.get('user_id')
+
+        if not id:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+
+    return token_data
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail='could not validate credentials',
+                                          headers={'WWW-Authenticate': 'Bearer'})
+    token = verify_access_token(token, credentials_exception)
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    return user
